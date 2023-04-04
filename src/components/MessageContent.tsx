71a39cc9737a7cfd1591ab0e27f509c7280fda44
yanjx21 <yjx2005@163.com>
2023-04-04 19:11:54 +0800

    #6-visual-实现了聊天窗的自动滚动

--- src/components/MessageContent.tsx
+++ src/components/MessageContent.tsx
@@ -1,23 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Message, Chat, ChatList } from '../utils/messagePage'
 import MessageItem from './MessageItem'
 
-const MessageContent = (props: {messages: Message[]}) => {
+const MessageContent = (props: { messages: Message[] }) => {
     const [messages, setMessages] = useState<Message[]>(props.messages)
+    const messagesEnd = useRef<HTMLDivElement>(null)
+
+    const scrollToBottom = () => {
+        if (messagesEnd && messagesEnd.current) {
+            messagesEnd.current.scrollIntoView({ behavior: 'smooth' })
+        }
+    }
     useEffect(() => {
         setMessages(props.messages)
+        scrollToBottom()
     }, [props.messages])
+    useEffect(() => {
+        scrollToBottom()
+    })
 
     return (
-        <div className="chat-content">
-            <div className="container-xxl">
-                <ul className="list-unstyled py-4">
-                    {messages.map((message) => (
-                        <MessageItem {...message} key={message.timestamp}/>
-                    ))}
-                </ul>
+        <>
+            <div className="chat-content">
+                <div className="container-xxl">
+                    <ul className="list-unstyled py-4">
+                        {messages.map((message) => (
+                            <MessageItem {...message} key={message.timestamp} />
+                        ))}
+                    </ul>
+                    <div
+                        style={{ clear: 'both', height: '1px', width: '100%' }}
+                        ref={messagesEnd}></div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Message, Chat, ChatList } from '../utils/messagePage'
 
-
 const MessageItem = (props: Message) => {
     const [message, setMessage] = useState<Message>(props)
     useEffect(() => {
@@ -9,19 +8,27 @@ const MessageItem = (props: Message) => {
     }, [props])
 
     return (
-        <li className={"d-flex message" + (message.isRight ? " right" : "")}>
-            {!message.isRight ? (<div className="mr-lg-3 me-2">
-                <div className="avatar rounded-circle no-image timber">
-                    <span>{message.senderId}</span>
+        <li className={'d-flex message' + (message.isRight ? ' right' : '')}>
+            {!message.isRight ? (
+                <div className="avatar mr-lg-3 me-2">
+                    <div
+                        //添加颜色
+                        className={'avatar rounded-circle no-image ' + ''}>
+                        <span>{message.senderId}</span>
+                    </div>
                 </div>
-            </div>) : ''}
+            ) : (
+                ''
+            )}
             <div className="message-body">
-                <span className="date-time text-muted">{(message.senderId ? (message.senderId + ', ') : '') +  new Date(message.timestamp).toLocaleString()}</span>
+                <span className="date-time text-muted">
+                    {(message.senderId ? message.senderId + ', ' : '') +
+                        new Date(message.timestamp).toLocaleString()}
+                </span>
                 <div
                     className={
-                        'message-row d-flex align-items-center' + (message.isRight
-                            ? ' justify-content-end'
-                            : '')
+                        'message-row d-flex align-items-center' +
+                        (message.isRight ? ' justify-content-end' : '')
                     }>
                     <div className={'message-content p-3' + (message.isRight ? ' border' : '')}>
                         {message.text}
